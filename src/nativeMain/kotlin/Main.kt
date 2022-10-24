3305d1d8d53ab8b7b3f2655dc9cf82925a2ff605
Charles Bourget <cbourget@stingray.com>
2022-10-24 11:11:59 -0400

    Add feature to search for multiple different patterns

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,32 +1,43 @@
 import platform.posix.exit
 
 const val ANSI_RED_BACKGROUND = "\u001B[41m"
+const val ANSI_GREEN_BACKGROUND = "\u001B[42m"
+const val ANSI_YELLOW_BACKGROUND = "\u001B[43m"
+const val ANSI_BLUE_BACKGROUND = "\u001B[44m"
+const val ANSI_PURPLE_BACKGROUND = "\u001B[45m"
+const val ANSI_CYAN_BACKGROUND = "\u001B[46m"
+const val ANSI_WHITE_BACKGROUND = "\u001B[47m"
 const val ANSI_RESET = "\u001B[0m"
 
+data class Pattern(val regex: Regex, val color: String)
+
 fun main(args: Array<String>) {
-    if (args.size != 1) {
-        println("ERROR: Program only accepts 1 positional argument")
-        usage()
-        exit(1)
-    }
+    val colors = listOf(
+        ANSI_RED_BACKGROUND,
+        ANSI_GREEN_BACKGROUND,
+        ANSI_YELLOW_BACKGROUND,
+        ANSI_BLUE_BACKGROUND,
+        ANSI_PURPLE_BACKGROUND,
+        ANSI_CYAN_BACKGROUND,
+        ANSI_WHITE_BACKGROUND
+    )
 
-    if (args[0] == "-h") {
-        usage()
-        exit(0)
-    }
+    validateArgs(args, colors)
 
-    val pattern = args[0]
-    val regex = Regex(pattern)
+    val patterns = List(args.size) { i -> Pattern(Regex(args[i]), colors[i]) }
 
     while (true) {
         try {
-            val line = readln()
+            var outputLine = readln()
 
-            if (regex.containsMatchIn(line)) {
-                println(ANSI_RED_BACKGROUND + line + ANSI_RESET)
-            } else {
-                println(line)
+            for (pattern in patterns) {
+                if (pattern.regex.containsMatchIn(outputLine)) {
+                    outputLine = pattern.color + outputLine + ANSI_RESET
+                    break
+                }
             }
+
+            println(outputLine)
         } catch (e: RuntimeException) {
             exit(0)
         }
@@ -35,4 +46,23 @@ fun main(args: Array<String>) {
 
 fun usage() {
     println("Usage: colorer PATTERN")
+}
+
+fun validateArgs(args: Array<String>, colors: List<String>) {
+    if (args.isEmpty()) {
+        println("ERROR: Program needs at least 1 positional argument")
+        usage()
+        exit(1)
+    }
+
+    if (args.contains("-h") || args.contains("--help")) {
+        usage()
+        exit(0)
+    }
+
+    if (args.size > colors.size) {
+        println("ERROR: Too much patterns. The limit is ${colors.size}")
+        usage()
+        exit(1)
+    }
 }
